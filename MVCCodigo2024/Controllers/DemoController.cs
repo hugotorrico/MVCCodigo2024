f89ab77a98b8230f294604245ebeb2e56711087c
Hugo Torrico <torricomarquezh@gmail.com>
2024-08-09 22:45:16 -0500

    Agregar archivos de proyecto.

---
+++ MVCCodigo2024/Controllers/DemoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCodigo2024.Controllers
+{
+    public class DemoController : Controller
+    {
+        //ActionResult=>html
+        public string HolaMundo()
+        {
+            return "<h1>Hola Mundo</h1>";
+        }
+        public string HolaCodigo()
+        {
+            return "<h1>Hola Codigo</h1>";
+        }
+
+        public string Formulario()
+        {
+            string simpleForm = @"
+<form action='/submit' method='post'>
+    <label for='name'>Name:</label><br>
+    <input type='text' id='name' name='name'><br><br>
+    <label for='email'>Email:</label><br>
+    <input type='email' id='email' name='email'><br><br>
+    <input type='submit' value='Submit'>
+</form>";
+
+            return simpleForm;
+        }
+
+        // GET: Demo
+        public ActionResult Index()
+        {
+            Session["Mensaje"] = "SOY EL PROFESOR HUGO TORRICO";
+
+            ViewBag.Mensaje = "Bienvenido a Codigo";
+            ViewBag.FechaHora = DateTime.Now.ToLongDateString();
+
+            if (DateTime.Now.Hour > 13)
+                ViewBag.Saludo = "Buenas Tardes";
+            else
+                ViewBag.Saludo = "Buenas Días";
+
+
+
+            return View();
+        }
+
+        // GET: Demo
+        public ActionResult ListarPersonas()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ MVCCodigo2024/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("MVCCodigo2024")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCCodigo2024")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("8b5b105f-9879-4f6a-b5d8-4827a9101e3e")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
