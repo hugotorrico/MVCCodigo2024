1560e524484665cdeaa59e0ac03333c91a1b81da
Hugo Torrico <torricomarquezh@gmail.com>
2024-08-12 21:43:04 -0500

    Listar producto modelo

--- MVCCodigo2024/Controllers/ProductosClientesController.cs
+++ MVCCodigo2024/Controllers/ProductosClientesController.cs
@@ -14,6 +14,25 @@ namespace MVCCodigo2024.Controllers
         {
             ProductoCliente model = new ProductoCliente();
 
+
+            List<Producto> productos = new List<Producto>
+        {
+            new Producto { Id = 1, Nombre = "Laptop", Precio = 1200.50m, Cantidad = 5 },
+            new Producto { Id = 2, Nombre = "Smartphone", Precio = 800.00m, Cantidad = 10 },
+            new Producto { Id = 3, Nombre = "Tablet", Precio = 400.00m, Cantidad = 7 }
+        };
+
+            List<Cliente> clientes = new List<Cliente>
+        {
+            new Cliente { Id = 1,  Nombres="Hugo Torrico" },
+            new Cliente { Id = 2,  Nombres="Juan Torrico"  },
+            new Cliente { Id = 3,  Nombres="Yury  Torrico" }
+        };
+
+
+            model.Clientes = clientes;
+            model.Productos = productos;
+
             return View(model);
         }
     }
