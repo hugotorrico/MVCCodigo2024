ce8c4875c00b870c39b471493319463ba3de347d
Hugo Torrico <torricomarquezh@gmail.com>
2024-08-16 19:38:44 -0500

    Partial Views

---
+++ MvcCodigoPartials/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using MvcCodigoPartials.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCodigoPartials.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            Usuario usuario = new Usuario();
+            return View(usuario);
+        }
+        public ActionResult Edit()
+        {
+            Usuario usuario = new Usuario();
+            return View(usuario);
+        }
+    }
+}
---
+++ MvcCodigoPartials/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcCodigoPartials.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+       
+        public string Nombre { get; set; }
+
+      
+        public string Email { get; set; }
+    }
+}
